09efade75d3cedc73f70f0966609156ecc032dbf
andresnino <andres_garcia1996@hotmail.com>
2018-07-15 22:47:29 -0500

    reportePDF.py

---
+++ Generar reporte en PDF/reportePDF.py
@@ -0,0 +1,205 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Nombre:       reportePDF.py
+# Autor:        Miguel Andres Garcia Niño
+# Creado:       15 de Julio 2018
+# Modificado:   15 de Julio 2018
+# Copyright:    (c) 2018 by Miguel Andres Garcia Niño, 2018
+# License:      Apache License 2.0
+# ----------------------------------------------------------------------------
+
+__versión__ = "1.0"
+
+# Versión Python: 3.5.2
+
+"""
+El módulo *reportePDF* permite crear un reporte PDF sencillo.
+"""
+
+from sqlite3 import connect
+
+from arrow import utcnow, get
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.units import inch, mm
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import Paragraph, SimpleDocTemplate, Spacer, Table, TableStyle
+from reportlab.lib.enums import TA_LEFT, TA_CENTER, TA_RIGHT
+from reportlab.lib.colors import black, purple, white
+from reportlab.pdfgen import canvas
+
+
+# ======================= CLASE reportePDF =========================
+
+class reportePDF(object):
+    """Exportar una lista de diccionarios a una tabla en un
+       archivo PDF."""
+    
+    def __init__(self, titulo, cabecera, datos, nombrePDF):
+        super(reportePDF, self).__init__()
+
+        self.titulo = titulo
+        self.cabecera = cabecera
+        self.datos = datos
+        self.nombrePDF = nombrePDF
+
+        self.estilos = getSampleStyleSheet()
+
+    @staticmethod
+    def _encabezadoPiePagina(canvas, archivoPDF):
+        """Guarde el estado de nuestro lienzo para que podamos aprovecharlo"""
+        
+        canvas.saveState()
+        estilos = getSampleStyleSheet()
+
+        alineacion = ParagraphStyle(name="alineacion", alignment=TA_RIGHT,
+                                    parent=estilos["Normal"])
+ 
+        # Encabezado
+        encabezadoNombre = Paragraph("Andres Niño 1.0", estilos["Normal"])
+        anchura, altura = encabezadoNombre.wrap(archivoPDF.width, archivoPDF.topMargin)
+        encabezadoNombre.drawOn(canvas, archivoPDF.leftMargin, 736)
+
+        fecha = utcnow().to("local").format("dddd, DD - MMMM - YYYY", locale="es")
+        fechaReporte = fecha.replace("-", "de")
+
+        encabezadoFecha = Paragraph(fechaReporte, alineacion)
+        anchura, altura = encabezadoFecha.wrap(archivoPDF.width, archivoPDF.topMargin)
+        encabezadoFecha.drawOn(canvas, archivoPDF.leftMargin, 736)
+ 
+        # Pie de página
+        piePagina = Paragraph("Reporte generado por Andres Niño.", estilos["Normal"])
+        anchura, altura = piePagina.wrap(archivoPDF.width, archivoPDF.bottomMargin)
+        piePagina.drawOn(canvas, archivoPDF.leftMargin, 15 * mm + (0.2 * inch))
+ 
+        # Suelta el lienzo
+        canvas.restoreState()
+
+    def convertirDatos(self):
+        """Convertir la lista de diccionarios a una lista de listas para crear
+           la tabla PDF."""
+
+        estiloEncabezado = ParagraphStyle(name="estiloEncabezado", alignment=TA_LEFT,
+                                          fontSize=10, textColor=white,
+                                          fontName="Helvetica-Bold",
+                                          parent=self.estilos["Normal"])
+
+        estiloNormal = self.estilos["Normal"]
+        estiloNormal.alignment = TA_LEFT
+
+        claves, nombres = zip(*[[k, n] for k, n in self.cabecera])
+
+        encabezado = [Paragraph(nombre, estiloEncabezado) for nombre in nombres]
+        nuevosDatos = [tuple(encabezado)]
+
+        for dato in self.datos:
+            nuevosDatos.append([Paragraph(str(dato[clave]), estiloNormal) for clave in claves])
+            
+        return nuevosDatos
+        
+    def Exportar(self):
+        """Exportar los datos a un archivo PDF."""
+
+        alineacionTitulo = ParagraphStyle(name="centrar", alignment=TA_CENTER, fontSize=13,
+                                          leading=10, textColor=purple,
+                                          parent=self.estilos["Heading1"])
+
+        convertirDatos = self.convertirDatos()
+        
+        tabla = Table(convertirDatos, hAlign="LEFT")
+        tabla.setStyle(TableStyle([
+            ("BACKGROUND", (0, 0),(-1, 0), purple),
+            ("ALIGN", (0, 0),(0, -1), "LEFT"),
+            ("VALIGN", (0, 0), (-1, -1), "MIDDLE"), # Texto centrado y alineado a la izquierda
+            ("INNERGRID", (0, 0), (-1, -1), 0.50, black), # Lineas internas
+            ("BOX", (0, 0), (-1, -1), 0.25, black), # Linea (Marco) externa
+            ]))
+
+        historia = []
+        historia.append(Paragraph(self.titulo, alineacionTitulo))
+        historia.append(Spacer(1, 0.16 * inch))
+        historia.append(tabla)
+
+        archivoPDF = SimpleDocTemplate(self.nombrePDF, rightMargin=50, pagesize=letter,
+                                       title="Reporte PDF", author="Andres Niño")
+        
+        try:
+            archivoPDF.build(historia, onFirstPage=self._encabezadoPiePagina,
+                             onLaterPages=self._encabezadoPiePagina,
+                             canvasmaker=numeracionPaginas)
+            
+         # +------------------------------------+
+            return "Reporte generado con éxito."
+         # +------------------------------------+
+        except PermissionError:
+         # +--------------------------------------------+  
+            return "Error inesperado: Permiso denegado."
+         # +--------------------------------------------+
+
+
+# ================== CLASE numeracionPaginas =======================
+
+class numeracionPaginas(canvas.Canvas):
+    def __init__(self, *args, **kwargs):
+        canvas.Canvas.__init__(self, *args, **kwargs)
+        self._saved_page_states = []
+
+    def showPage(self):
+        self._saved_page_states.append(dict(self.__dict__))
+        self._startPage()
+
+    def save(self):
+        """Agregar información de la página a cada página (página x de y)"""
+        numeroPaginas = len(self._saved_page_states)
+        for state in self._saved_page_states:
+            self.__dict__.update(state)
+            self.draw_page_number(numeroPaginas)
+            canvas.Canvas.showPage(self)
+        canvas.Canvas.save(self)
+ 
+    def draw_page_number(self, conteoPaginas):
+        self.drawRightString(204 * mm, 15 * mm + (0.2 * inch),
+                             "Página {} de {}".format(self._pageNumber, conteoPaginas))        
+
+
+# ===================== FUNCIÓN generarReporte =====================
+
+def generarReporte():
+    """Ejecutar consulta a la base de datos (DB_USUARIOS) y llamar la función Exportar, la
+       cuál esta en la clase reportePDF, a esta clase le pasamos el título de la tabla, la
+       cabecera y los datos que llevará."""
+    
+    def dict_factory(cursor, row):
+        d = {}
+        for idx, col in enumerate(cursor.description):
+            d[col[0]] = row[idx]
+        return d
+
+    conexionDB = connect("DB_USUARIOS.db")
+    conexionDB.row_factory = dict_factory # Forma avanzada de obtener resultados
+    cursor = conexionDB.cursor()
+
+    cursor.execute("SELECT DNI, NOMBRE, APELLIDO, FECHA_NACIMIENTO FROM USUARIOS")
+    datos = cursor.fetchall()
+    # datos = [{"DNI": "1110800310", "NOMBRE": "Andres", "APELLIDO": "Niño", "FECHA_NACIMIENTO": "06/06/2019"},
+    #          {"DNI": "1110800311", "NOMBRE": "Andres", "APELLIDO": "Niño", "FECHA_NACIMIENTO": "06/06/2019"}]
+
+    conexionDB.close()
+
+    titulo = "LISTADO DE USUARIOS"
+
+    cabecera = (
+        ("DNI", "D.N.I"),
+        ("NOMBRE", "NOMBRE"),
+        ("APELLIDO", "APELLIDO"),
+        ("FECHA_NACIMIENTO", "FECHA DE NACIMIENTO"),
+        )
+
+    nombrePDF = "Listado de usuarios.pdf"
+
+    reporte = reportePDF(titulo, cabecera, datos, nombrePDF).Exportar()
+    print(reporte)
+
+
+# ======================== LLAMAR FUNCIÓN ==========================
+
+generarReporte()
